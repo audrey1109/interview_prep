ce250c4bea8f530cffde03bf322e0f151e026dcd
Audrey Palmer <audreypuke@doxxing-machine-3.local>
2025-09-02 10:46:11 -0500

    added functionality so the button now submits the comments

--- website/views.py
+++ website/views.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template, redirect, url_for
-from flask import request
+from flask import Blueprint, render_template, jsonify, redirect, request, url_for
 import requests
 import os
+
+from .database import db
+from .models import AnonComments
 
 # Create a blueprint
 main_blueprint = Blueprint('main', __name__)
@@ -9,3 +11,16 @@ main_blueprint = Blueprint('main', __name__)
 @main_blueprint.route('/', methods=['GET', 'POST'])
 def main_page():
     return render_template('homepage.html')
+
+@main_blueprint.route("/submit_comment", methods = ["POST"])
+def submit_comment() :
+
+    comment = request.get_json().get("comment")
+
+    anon_comment = AnonComments(text = comment)
+    db.session.add(anon_comment)
+    db.session.commit()
+
+    return jsonify({"status": "success", "comment": comment})
+
+    
